6f9c77ac32367820ba07fa46f5e20fd824590d5a
SHARJEEL-DS <sharjeelsohail697@gmai.com>
2023-05-29 08:31:49 +0500

    project setup

---
+++ pages/index.jsx
@@ -0,0 +1,34 @@
+import Head from "next/head";
+import Hero from "@/components/Hero";
+import RealState from "@/components/RealState";
+
+import Category from "@/components/Category";
+import Entry from "@/components/Entry";
+import Personalised from "@/components/Personalised ";
+
+export default function Home() {
+  
+
+  return (
+    <>
+      <Head>
+        <title>sharjeel</title>
+      </Head>
+
+      <main>
+      <div className=" h-screen">
+     
+     
+     <Hero/>
+     <RealState/>
+     <Category/>
+     <Entry/>
+    <Personalised/>
+    
+    
+     
+    </div>
+      </main>
+    </>
+  );
+}
